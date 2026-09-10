b65496d039b3eefca061f990b71aa592bb2f986d
textbytext <alferovvictor@gmail.com>
2026-09-10 16:46:24 +0200

    Add 3 tests for calculate_average method to test_helpers.py

--- src/tests/test_helpers.py
+++ src/tests/test_helpers.py
@@ -1,4 +1,4 @@
-from promts.helpers import odd_numbers_and_average
+from promts.helpers import odd_numbers_and_average, calculate_average
 
 def test_odd_numbers_and_average_returns_odds_and_average():
 	assert odd_numbers_and_average([1, 2, 3, 4, 5]) == ([1, 3, 5], 3.0)
@@ -10,3 +10,16 @@ def test_odd_numbers_and_average_returns_none_for_no_odd_numbers():
 
 def test_odd_numbers_and_average_handles_empty_input():
 	assert odd_numbers_and_average([]) == ([], None)
+
+
+def test_calculate_average_returns_average_of_positive_numbers():
+	assert calculate_average([1, 2, 3, 4, 5]) == 3.0
+
+
+def test_calculate_average_returns_average_of_decimals():
+	assert calculate_average([1.5, 2.5, 3.0]) == 2.333333333333333
+
+
+def test_calculate_average_returns_none_for_empty_list():
+	assert calculate_average([]) is None
+
